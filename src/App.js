d26c998dee7ce56fa3c132fe63ec85fc6ed85c69
jennywoon <cjaeks0394@naver.com>
2022-08-06 16:51:38 +0900

    참고 주석 추가

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-// import Router from './shared/Router';
+import Router from './shared/Router';
 
 function App() {
-  // return <Router />;
-  return <div>안녕</div>
+  return <Router />;
+
 }
 
 export default App;
---
+++ src/shared/Router.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import TodoList from "../pages/TodoList"
+import TodoDetail from "../pages/TodoDetail"
+import TodoForm from "../pages/TodoForm"
+
+const Router = () => {
+    return(
+        <BrowserRouter>
+            <Routes>
+                <Route path ="/" element={<TodoList/>}/>
+                <Route path ="/detail/:id" element={<TodoDetail/>}/>
+                <Route path ="/form" element={<TodoForm/>}/>
+            </Routes>
+        </BrowserRouter>
+    )
+}
+export default Router;
